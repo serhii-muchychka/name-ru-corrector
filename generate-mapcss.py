9c94aad301f3b1c561df5daeb203bcf70643608a
Serhii Muchychka <Serhii.Muchychka@gmail.com>
2023-11-01 09:09:45 +0200

    Simplify code

--- generate-mapcss.py
+++ generate-mapcss.py
@@ -1,10 +1,8 @@
-import codecs
-
-with codecs.open("ua-name-ru.validator.mapcss", mode="w", encoding="utf-8") as mapcss_file:
-  with codecs.open("ua-name-ru.validator.mapcss.head.txt", encoding="utf-8") as head_file:
+with open("ua-name-ru.validator.mapcss", mode="w", encoding="utf-8") as mapcss_file:
+  with open("ua-name-ru.validator.mapcss.head.txt", encoding="utf-8") as head_file:
     mapcss_file.write(head_file.read())
 
-  with codecs.open("translations.txt", encoding='utf-8') as translations_file:
+  with open("translations.txt", encoding='utf-8') as translations_file:
     for line in translations_file:
       name_uk, name_ru = line.strip().split(';', 2)
       pattern = '''*["name:ru"][name="{0}"]["name:ru"!="{1}"].base_condition {{

60b2fc25e3523a9e26f54904fba06d41eabb3ecc
Serhii Muchychka <Serhii.Muchychka@gmail.com>
2023-11-20 16:31:13 +0200

    Add command-line options

--- generate-mapcss.py
+++ generate-mapcss.py
@@ -1,14 +1,19 @@
-with open("ua-name-ru.validator.mapcss", mode="w", encoding="utf-8") as mapcss_file:
-  with open("ua-name-ru.validator.mapcss.head.txt", encoding="utf-8") as head_file:
-    mapcss_file.write(head_file.read())
+import argparse
 
-  with open("translations.txt", encoding='utf-8') as translations_file:
-    for line in translations_file:
-      name_uk, name_ru = line.strip().split(';', 2)
-      pattern = '''*["name:ru"][name="{0}"]["name:ru"!="{1}"].base_condition {{
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--translations", default="translations/translations.txt", type=argparse.FileType(encoding="utf-8"))
+parser.add_argument("-o", "--output", default="ua-name-ru.validator.mapcss", type=argparse.FileType(mode='w', encoding="utf-8"))
+parsed_args = parser.parse_args()
+
+with open("ua-name-ru.validator.mapcss.head.txt", encoding="utf-8") as head_file:
+  parsed_args.output.write(head_file.read())
+
+for line in parsed_args.translations:
+  name_uk, name_ru = line.strip().split(';', 2)
+  pattern = '''*["name:ru"][name="{0}"]["name:ru"!="{1}"].base_condition {{
   throwWarning: tr("У назві «{0}» name:ru повинен бути «{1}» замість «{{0}}»", "{{0.value}}");
   group: tr("Російська назва не відповідає правилам перекладу власних назв");
   fixAdd: "name:ru={1}";
 }}
 '''
-      mapcss_file.write(pattern.format(name_uk, name_ru))
+  parsed_args.output.write(pattern.format(name_uk, name_ru))
